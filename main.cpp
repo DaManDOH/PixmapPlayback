08d0195f8dc28e1a31e90bbeec5bf75b25097df9
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2024-01-29 17:56:58 -0500

    Simplified example

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowFlag(Qt::Dialog);
-    w.setWindowFlag(Qt::WindowMaximizeButtonHint, false);
     w.showMaximized();
     return a.exec();
 }
